68afb112f3d60cc40de94397dba3e3351b261636
jiangwenke <jiangwenke@qding.me>
2020-05-30 15:10:29 +0800

    初始化

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<!--
+ * @Author: your name
+ * @Date: 2020-05-30 11:09:27
+ * @LastEditTime: 2020-05-30 11:31:04
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: /vue-admin-template/src/App.vue
+--> 
+
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
---
+++ src/main.js
@@ -0,0 +1,54 @@
+/*
+ * @Author: Jiang wenke
+ * @LastEditors: Jiang wenke
+ * @email: wenkejiang@yeah.net
+ * @github: https://github.com/wenkejiang/
+ * @Date: 2020-05-30 11:09:27
+ * @LastEditTime: 2020-05-30 14:53:36
+ * @motto: Still water run deep
+ * @Description: Modify here please
+ * @FilePath: /vue-admin-template/src/main.js
+ */
+import Vue from 'vue'
+
+import 'normalize.css/normalize.css' // A modern alternative to CSS resets
+
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+// import locale from 'element-ui/lib/locale/lang/en' // lang i18n
+
+import '@/styles/index.scss' // global css
+
+import App from './App'
+import store from './store'
+import router from './router'
+
+import '@/icons' // icon
+import '@/permission' // permission control
+
+/**
+ * If you don't want to use mock-server
+ * you want to use MockJs for mock api
+ * you can execute: mockXHR()
+ *
+ * Currently MockJs will be used in the production environment,
+ * please remove it before going online ! ! !
+ */
+if (process.env.NODE_ENV === 'production') {
+  const { mockXHR } = require('../mock')
+  mockXHR()
+}
+
+// set ElementUI lang to EN
+// Vue.use(ElementUI, { locale })
+// 如果想要中文版 element-ui，按如下方式声明
+Vue.use(ElementUI)
+
+Vue.config.productionTip = false
+
+new Vue({
+  el: '#app',
+  router,
+  store,
+  render: h => h(App)
+})
